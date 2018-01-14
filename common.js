f16ea8aa0167611d79658b6fe75c258491e01850
Jane Lee <1217359987@qq.com>
2018-01-14 14:41:57 +0800

    Update common.js
    添加完整的事件监听函数

--- common.js
+++ common.js
@@ -202,4 +202,72 @@ function stopDefault(e){
     }
 }
 
+////////////////////////////////////////////////////////////
 
+作者：不落誓言
+链接：https://www.jianshu.com/p/5faa5aceeaef
+來源：简书
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// 把所有方法封装到一个对象里面，充分考虑兼容写法
+var EventUtil = {
+  // 添加DOM事件
+  addEvent: function(element, type, handler) {
+    if(element.addEventListener) { //DOM2级
+      element.addEventListener(type, handler, false);//false表示该元素在事件的“冒泡阶段”（由内向外传递时）响应事件。
+    }else if(element.attachEvent) {  //IE
+      element.attachEvent("on"+ type, handler);
+    }else {
+      element["on" + type] = handler;
+    }
+  },
+  // 移除DOM事件
+  removeEvent: function(element, type, handler) {
+    if(element.removeEventListener) { //DOM2级
+      element.removeEventListener(type, handler, false);
+    }else if(element.detachEvent) {  //IE
+      element.detachEvent("on"+ type, handler);
+    }else {
+      element["on" + type] = null;
+    }
+  },
+  // 阻止事件冒泡
+  stopPropagation: function(ev) {
+    if(ev.stopPropagation) {
+      ev.stopPropagation();
+    }else { //IE
+      ev.cancelBubble = true;
+    }
+  },
+  // 阻止默认事件
+  preventDefault: function(ev) {
+    if(ev.preventDefault) {
+      ev.preventDefaule();
+    }else {//IE
+      ev.returnValue = false;
+    }
+  },
+  // 获取事件源对象
+  getTarget: function(ev) {
+    return event.target || event.srcElement; //target--FF,srcElement--IE,chrome两者都有
+  },
+  // 获取事件对象
+  getEvent: function(e) {
+    var ev = e || window.event; //window.event--IE
+    if(!ev) {
+      var c = this.getEvent.caller;
+      while(c) {
+        ev = c.arguments[0];
+        if(ev && Event == ev.constructor) {
+          break;
+        }
+        c = c.caller;
+      }
+    }
+    return ev;
+  }
+};
+
+作者：不落誓言
+链接：https://www.jianshu.com/p/5faa5aceeaef
+來源：简书
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
